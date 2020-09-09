5fb0262c7ef401332b711f1db151ca412d12d462
Hafsa-Iqbal <56120865+Hafsa-Iqbal@users.noreply.github.com>
2020-09-09 19:33:00 +0200

    modify FeaturesExtraction file

--- FeaturesExtraction.m
+++ FeaturesExtraction.m
@@ -1,59 +1,102 @@
- clc
+% clc
 clear all
 close all
+tuneNetwork = true;
 
-%% load pre-trained modelnet40 network to extract feature
+%% load pre-trained modelnet40 network 
 load('modelnet40PretrainNetwork.mat')
-%% upload merge data
-path = '.\datasets\MergeSydney_data/';
-pcds = dataSydneyReadFuction(path);   % augmentation and voxelization
-dataTrain =  pcds ;
 
-%% plot  voxels
-% dataPreview = preview(pcds);
-% figure
-% p = patch(isosurface(dataPreview,0.5));
-% p.FaceColor = 'red';
-% p.EdgeColor = 'none';
-% daspect([1 1 1])
-% view(45,45)
-% camlight; 
-% lighting phong
+%% upload data and label for transfer learning
+if tuneNetwork == true
+    pathTrain = '.\datasets\MergeSydney_data\train/';
+    pcds = dataSydneyReadFuction(pathTrain);  
+    dataTrain =  pcds ;
+    
+    pathValidated = '.\datasets\MergeSydney_data\test/';
+    pcds = dataSydneyReadFuction(pathValidated);   
+    dataValidated =  pcds ;
+    % load training and testing labels
+    load('LabelsMergeTrain.mat')
+    traininglabels = categorical(class_sydneyMergeTrain);
+    load('LabelsMergeTest.mat')
+    validationlabels = categorical(class_sydneyMergeTest);
+else
+    pathTrain = '.\datasets\sydney_txt_files\train/'; 
+    pcds = dataSydneyReadFuction(pathTrain);   
+    dataTrain =  pcds ;
+    
+    pathValidated = '.\datasets\sydney_txt_files\test/';
+    pcds = dataSydneyReadFuction(pathValidated);  
+    dataValidated =  pcds ;
+    % load traing and testing labels
+    load('LabelsSydneyTrain.mat')
+    traininglabels = categorical(class_sydneyTrain);
+    load('LabelsSydneyTest.mat')
+    validationlabels = categorical(class_sydneyTest);
+    
+end
 
-% load labels 
-load('LabelsSydney.mat')
-traininglabels = categorical(class_sydney);
+% update layers 
+numFeatures = 1024;
+numClasses = 3;
+layers = voxnet.Layers;
+layers = layers(1:end-3);
+layers(end+1) = fullyConnectedLayer(numFeatures,'Name','fc2');
+layers(end+1) = reluLayer('Name','relu1');
+layers(end+1) = fullyConnectedLayer(numClasses,'Name','fc3');
+layers(end+1) = softmaxLayer('Name','softmax');
+layers(end+1) = classificationLayer('Name','crossEntropyLoss');
 
-% feature extraction layer
-featureLayer = 'fc1';
+voxnet_update = layerGraph(layers);
 
-% perform feature extraction
-trainingFeatures = activations(voxnet, dataTrain, featureLayer, 'OutputAs', 'columns');
+% training options
+miniBatchSize = 32;
+dsLength = length(dataTrain.Files);
+iterationsPerEpoch = floor(dsLength/miniBatchSize);
+dropPeriod = floor(8000/iterationsPerEpoch);
 
-% train classifier
-classifier = fitcecoc(trainingFeatures',traininglabels);
+options_2 = trainingOptions('sgdm','InitialLearnRate',0.01,'MiniBatchSize',miniBatchSize,...
+    'LearnRateSchedule','Piecewise',...
+    'LearnRateDropPeriod',dropPeriod,...
+    'ValidationData',dataValidated,'MaxEpochs',60,...
+    'DispatchInBackground',false,...
+    'Plots','training-progress',...
+    'Shuffle','never');
 
-%% load clustered txt files
-path = '.\datasets\iCAB data';  
-% data handling, ore-processing and voxelization 
+% train network
+voxnet_update = trainNetwork(dataTrain,voxnet_update,options_2);
+
+% Evaluate network
+
+outputLabels = classify(voxnet_update,dataValidated);
+accuracy = nnz(outputLabels == validationlabels) / numel(outputLabels);
+disp(accuracy)
+
+
+
+%% load icab data 
+path = '.\datasets\iCAB_data';
+% data handling, pre-processing and voxelization
 grid_vox = 32;
-pcds = dataPC(path,grid_vox); 
-testingSet = pcds;
+pcds = dataPC(path,grid_vox);
+testingdataset = pcds;
 
 %% plot voxels
-dataPreview = preview(testingSet);
+dataPreview = preview(testingdataset);
 figure
 p = patch(isosurface(dataPreview,0.5));
 p.FaceColor = 'red';
 p.EdgeColor = 'none';
 daspect([1 1 1])
 view(45,45)
-camlight; 
+camlight;
 lighting phong
-%%
+%% feature extraction 
+
+% feature extraction layer
+featureLayer = 'fc2';
 % feature extraction
-testingFeatures = activations(voxnet, testingSet, featureLayer, 'OutputAs', 'columns');
-% classify point clouds
-classifierTest = predict(classifier,testingFeatures');
+testingFeatures = activations(voxnet_update, testingdataset, featureLayer, 'OutputAs', 'columns');
+
 % save features
 save('Features.mat','testingFeatures')
